263df477299eaae3e26567242bd3e3af04c6343a
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-28 07:09:53 +0600

    conditional Show All Button added

--- js/phone.js
+++ js/phone.js
@@ -13,6 +13,16 @@ const displayPhones = phones => {
     const phoneContainer = document.getElementById('phone-container');
     // clear phone container card before adding new card;
     phoneContainer.textContent = ""
+
+    // display show all button if there  are more than 12 phone 
+    const showAllContainer = document.getElementById('show-all-container');
+    if (phones.length > 12) {
+        showAllContainer.classList.remove('hidden')
+    } else { showAllContainer.classList.add('hidden') }
+
+
+    // display only first 10 phones;
+    phones = phones.slice(0, 12);
     phones.forEach(phone => {
         console.log(phone);
         // 2 create a div
